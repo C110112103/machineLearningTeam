38674d54c87620994c927346b8010d3ab3cdaa4a
a147852z <71688412+a147852z@users.noreply.github.com>
2024-06-12 17:07:46 +0800

    Update ml_play_manual.py

--- tank/Tank_code/ml_play_manual.py
+++ tank/Tank_code/ml_play_manual.py
@@ -18,11 +18,19 @@ class MLPlay:
         
         if self.side == "1P":
             P1 = scene_info['teammate_info'][0]
-            P2 = scene_info['oil_stations_info'][1]
+
+            data = scene_info['oil_stations_info']
+
+            max_x = data[0]
+            for item in data:
+                if item['x'] > max_x['x']:
+                    max_x = item  
+
+            P2 = max_x
             command = []
             if scene_info['power'] > 0:
-                if abs(P1['x'] - P2['x']) > 25 or abs(P1['y'] - P2['y']) > 25:
-                    if abs(P2['x'] - P1['x']) - abs(P2['y'] - P1['y']) > 0:
+                if abs(P1['x'] - P2['x']) > 5 or abs(P1['y'] - P2['y']) > 5:
+                    if abs(P2['x'] - P1['x'])> 8:
                         # command = []
                         if P2['x'] - P1['x'] < 0:
                             turn = int((0 - P1['angle']) / 45)
@@ -41,36 +49,27 @@ class MLPlay:
                         # if abs(P2['x'] - P1['x']) > 10:
                         #     command.append("FORWARD")
                                     
-                    elif abs(P2['y'] - P1['y']) > 25:    
+                    elif abs(P2['y'] - P1['y']) > 8:    
                         # command = []
                         if P2['y'] - P1['y'] < 0:
                             turn = int((270 - P1['angle']) / 45)
                             if turn >= 1 and turn <= 4:
                                     command.append("TURN_LEFT")
                             elif turn != 0:
-                                    command.append("TURN_RIGHT")                        
+                                    command.append("TURN_RIGHT")                      
                 
                         elif P2['y'] - P1['y'] > 0:
                             turn = int((90 - P1['angle']) / 45)
                             if turn >= -4 and turn <= -1:
                                     command.append("TURN_RIGHT")
                             elif turn != 0:
-                                    command.append("TURN_LEFT")       
+                                    command.append("TURN_LEFT")     
                                     
-                        # if abs(P2['y'] - P1['y']) > 10:
-                if abs(P1['x'] - P2['x']) > 25 or abs(P1['y'] - P2['y']) > 25:
-                    command.append("FORWARD")                                 
-                    
-            
-            if self.P1_pre == None:
-                command = []
-                command.append("SHOOT")
-                command.append("SHOOT")
-                command.append("SHOOT")
-                command.append("SHOOT")
-                command.append("SHOOT")
-                self.P1_pre = True
-                
+                        # if abs(P2['y'] - P1['y']) > 10:              
+                if (abs(P1['x'] - P2['x']) > 5 or abs(P1['y'] - P2['y']) > 5) and len(command) == 0:
+                    command.append("FORWARD")  
+                # if len(command) == 2:
+                #     del command[1]                        
             return command
 
         elif self.side == "2P":
@@ -89,7 +88,6 @@ class MLPlay:
 
             if not command:
                 command.append("NONE")
-            print(command)
             return command
 
     def reset(self):
