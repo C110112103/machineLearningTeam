657708b2f70bafdd3b53f4c6cef5b285c9697338
a147852z <71688412+a147852z@users.noreply.github.com>
2024-06-18 16:09:26 +0800

    Update ml_play_manual.py

--- tank/Tank_code/ml_play_manual.py
+++ tank/Tank_code/ml_play_manual.py
@@ -1,7 +1,9 @@
 import pygame
 import math
-
-
+import numpy as np
+import os
+import json
+import time
 
 class MLPlay:
     def __init__(self, ai_name, *args, **kwargs):
@@ -11,9 +13,30 @@ class MLPlay:
         self.ai = None  # 初始化时没有AI对象
         self.P1_pre = None
         self.P2_pre = None
+        self.P1_log = []
+        self.P2_log = []
 
     def update(self, scene_info: dict, keyboard=[], *args, **kwargs):
         if scene_info["status"] != "GAME_ALIVE":
+            print("exnd")
+            if len(self.P1_log) != 0:
+                log_folder = r"C:\Users\gslab\AppData\Local\paia_desktop\app-2.6.0\resources\app.asar.unpacked\games\TankMan\LOG"
+                if not os.path.exists(log_folder):
+                    os.makedirs(log_folder)
+                    
+                log_file_path = os.path.join(log_folder, f"{time.time()}.json")
+
+                try:
+                    with open(log_file_path, "r", encoding='utf-8') as log_file:
+                        log_data = json.load(log_file)
+                except FileNotFoundError:
+                    log_data = []
+
+                log_data = self.P1_log
+
+                with open(log_file_path, "w", encoding='utf-8') as log_file:
+                    json.dump(log_data, log_file, ensure_ascii=False, indent=4)
+                self.P1_log = []
             return "RESET"
         
         if self.side == "1P":
@@ -24,7 +47,7 @@ class MLPlay:
             for item in data:
                 if item['x'] > max_x['x']:
                     max_x = item  
-
+            
             P2 = max_x  
 
             if P1['oil'] < 30:
@@ -34,9 +57,10 @@ class MLPlay:
                 for item in data:
                     if item['x'] > max_x['x']:
                         max_x = item  
+                        
+                action = "oil"
 
                 P2 = max_x  
-                print(1)
 
             elif P1['power'] == 5:
                 data = scene_info['bullet_stations_info']
@@ -46,13 +70,15 @@ class MLPlay:
                     if item['x'] > max_x['x']:
                         max_x = item  
 
+                action = "power"
+
                 P2 = max_x      
 
 
             command = []
             if scene_info['power'] > 0:
                 if abs(P1['x'] - P2['x']) > 5 or abs(P1['y'] - P2['y']) > 5:
-                    if abs(P2['x'] - P1['x'])> 8:
+                    if abs(P2['x'] - P1['x'])> 10:
                         # command = []
                         if P2['x'] - P1['x'] < 0:
                             turn = int((0 - P1['angle']) / 45)
@@ -71,7 +97,7 @@ class MLPlay:
                         # if abs(P2['x'] - P1['x']) > 10:
                         #     command.append("FORWARD")
                                     
-                    elif abs(P2['y'] - P1['y']) > 8:    
+                    elif abs(P2['y'] - P1['y']) > 10:    
                         # command = []
                         if P2['y'] - P1['y'] < 0:
                             turn = int((270 - P1['angle']) / 45)
@@ -94,24 +120,89 @@ class MLPlay:
                 #     del command[1]                        
             if P1['power'] > 5:
                 command.append("SHOOT")
+                
+            action = np.array(command)
+            action = action[-1]
+            # print([[P1['x'], P1['y']], action, [P2['x'], P2['y']], P1['power']])
+            self.P1_log.append([[P1['x'], P1['y']], action, [P2['x'], P2['y']], P1['power']])
             return command
 
         elif self.side == "2P":
+            P2 = scene_info['teammate_info'][0]
+            data = scene_info['oil_stations_info']
+
+            max_x = data[0]
+            for item in data:
+                if item['x'] < max_x['x']:
+                    max_x = item  
+
+            P1 = max_x  
+
+            if P2['oil'] < 30:
+                data = scene_info['oil_stations_info']
+
+                max_x = data[0]
+                for item in data:
+                    if item['x'] < max_x['x']:
+                        max_x = item  
+
+                P1 = max_x  
+
+            elif P2['power'] == 5:
+                data = scene_info['bullet_stations_info']
+
+                max_x = data[0]
+                for item in data:
+                    if item['x'] < max_x['x']:
+                        max_x = item  
+
+                P1 = max_x      
+
             command = []
-            if pygame.K_d in keyboard:
-                command.append("TURN_RIGHT")
-            elif pygame.K_a in keyboard:
-                command.append("TURN_LEFT")
-            elif pygame.K_w in keyboard:
-                command.append("FORWARD")
-            elif pygame.K_s in keyboard:
-                command.append("BACKWARD")
-
-            if pygame.K_f in keyboard:
+            if scene_info['power'] > 0:
+                if abs(P2['x'] - P1['x']) > 5 or abs(P2['y'] - P1['y']) > 5:
+                    if abs(P1['x'] - P2['x'])> 10:
+                        # command = []
+                        if P1['x'] - P2['x'] < 0:
+                            turn = int((0 - P2['angle']) / 45)
+                            if turn >= -4 and turn <= -1:
+                                    command.append("TURN_RIGHT")
+                            elif turn != 0:
+                                    command.append("TURN_LEFT")
+                            
+                        elif P1['x'] - P2['x'] > 0:
+                            turn = int((180 - P2['angle']) / 45)
+                            if turn >= 1 and turn <= 4:
+                                    command.append("TURN_LEFT")
+                            elif turn != 0:
+                                    command.append("TURN_RIGHT")
+                                    
+                        # if abs(P2['x'] - P1['x']) > 10:
+                        #     command.append("FORWARD")
+                                    
+                    elif abs(P1['y'] - P2['y']) > 10:    
+                        # command = []
+                        if P1['y'] - P2['y'] < 0:
+                            turn = int((270 - P2['angle']) / 45)
+                            if turn >= 1 and turn <= 4:
+                                    command.append("TURN_LEFT")
+                            elif turn != 0:
+                                    command.append("TURN_RIGHT")                      
+                
+                        elif P1['y'] - P2['y'] > 0:
+                            turn = int((90 - P2['angle']) / 45)
+                            if turn >= -4 and turn <= -1:
+                                    command.append("TURN_RIGHT")
+                            elif turn != 0:
+                                    command.append("TURN_LEFT")     
+                                    
+                        # if abs(P2['y'] - P1['y']) > 10:              
+                if (abs(P2['x'] - P1['x']) > 5 or abs(P2['y'] - P1['y']) > 5) and len(command) == 0:
+                    command.append("FORWARD")  
+                # if len(command) == 2:
+                #     del command[1]                        
+            if P2['power'] > 5:
                 command.append("SHOOT")
-
-            if not command:
-                command.append("NONE")
             return command
 
     def reset(self):
